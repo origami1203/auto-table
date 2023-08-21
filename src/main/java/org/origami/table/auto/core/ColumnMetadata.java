9dfdb53f93516f8857993b931a7f8fbafc3b15d5
origami <mcxyml123@163.com>
2023-08-21 21:46:10 +0800

    refactor: column元数据添加自增，默认值，所属表等信息，更改lombok注解

--- src/main/java/org/origami/table/auto/core/ColumnMetadata.java
+++ src/main/java/org/origami/table/auto/core/ColumnMetadata.java
@@ -4,8 +4,6 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 import org.origami.table.auto.constant.Constants;
 
-import java.sql.JDBCType;
-
 /**
  * 数据列元数据
  *
@@ -13,56 +11,76 @@ import java.sql.JDBCType;
  * @date 2023/8/9 22:50
  */
 @Data
-@Accessors(chain = true, fluent = true)
+@Accessors(chain = true)
 public class ColumnMetadata {
-    
+
     /**
      * 列名
      */
     private String columnName;
-    
+
+    /**
+     * 列定义
+     */
+    private String columnDefinition;
+
+    /**
+     * jdbc类型
+     */
+    private Integer jdbcType;
+
     /**
      * 是否可为null
      */
     private Boolean nullable = Constants.DEFAULT_NULLABLE;
-    
+
     /**
      * 是否是主键
      */
     private Boolean primary = false;
-    
+
     /**
      * 备注
      */
     private String comment;
-    
+
     /**
      * 是否唯一
      */
     private Boolean unique;
-    
+
     /**
      * 不同数据库对于同一种类型可能有不同的叫法
      */
     private String actualTypeName;
-    
-    
+
+    /**
+     * 默认值
+     */
     private String defaultValue;
-    
+
+    /**
+     * 自增
+     */
     private Boolean isAutoIncrement;
-    
+
     /**
      * 长度,用于varchar，char，text等
      */
     private Integer length;
-    
+
     /**
      * 小数点位数
      */
     private Integer scale;
-    
+
     /**
      * 精度
      */
     private Integer precision;
+
+    /**
+     * 所属表
+     */
+    private TableMetadata table;
 }
