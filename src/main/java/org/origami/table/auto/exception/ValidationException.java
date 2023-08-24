201e19f5ef3debaf526afa8ce7aa3726b4ea6189
origami <mcxyml123@163.com>
2023-08-24 19:15:45 +0800

    feat: 新增exception

---
+++ src/main/java/org/origami/table/auto/exception/ValidationException.java
@@ -0,0 +1,13 @@
+package org.origami.table.auto.exception;
+
+/**
+ * @author origami
+ * @date 2023/8/23 19:26
+ */
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+}
