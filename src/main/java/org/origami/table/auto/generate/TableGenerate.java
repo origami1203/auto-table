8804a8133e4a3a9ac7d040d106e357b78d21d969
origami <mcxyml123@163.com>
2023-08-28 12:50:19 +0800

    refactor: schema策略获取修改，以后改为简单工厂获取

--- src/main/java/org/origami/table/auto/generate/TableGenerate.java
+++ src/main/java/org/origami/table/auto/generate/TableGenerate.java
@@ -4,18 +4,24 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Sets;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.origami.table.auto.annotations.Column;
 import org.origami.table.auto.annotations.Table;
 import org.origami.table.auto.config.properties.AutoTableProperties;
 import org.origami.table.auto.core.ColumnMetadata;
+import org.origami.table.auto.core.DatabaseMetadata;
 import org.origami.table.auto.core.TableMetadata;
 import org.origami.table.auto.dialect.Dialect;
 import org.origami.table.auto.dialect.MySQLDialect;
 import org.origami.table.auto.exception.UnsupportedDatabaseException;
 import org.origami.table.auto.resolve.ColumnManager;
 import org.origami.table.auto.resolve.TableManager;
-import org.origami.table.auto.utils.SqlHelper;
+import org.origami.table.auto.schema.SchemaContext;
+import org.origami.table.auto.schema.SchemaCreatorStrategyImpl;
+import org.origami.table.auto.schema.SchemaStrategy;
+import org.origami.table.auto.schema.SchemaUpdateStrategyImpl;
+import org.origami.table.auto.schema.SchemaValidateStrategyImpl;
 import org.reflections.ReflectionUtils;
 import org.reflections.Reflections;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +29,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
 import java.lang.reflect.Field;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
@@ -38,34 +47,39 @@ import java.util.Set;
 @Component
 @EnableConfigurationProperties(AutoTableProperties.class)
 public class TableGenerate {
-    
+
     @Autowired
     private AutoTableProperties properties;
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    
-    @SuppressWarnings("unchecked")
+
+    @SneakyThrows
     public void generate() {
-        
+
         String ddlAuto = properties.getDdlAuto();
-        if ("none".equalsIgnoreCase(ddlAuto)) {
+
+        SchemaStrategy schemaStrategy = getSchemaStrategy(ddlAuto);
+
+        if (schemaStrategy == null) {
             return;
         }
+        log.info("当前ddl-auto为[{}]", ddlAuto);
+        SchemaContext schemaContext = SchemaContext.of(schemaStrategy, jdbcTemplate);
+        DatabaseMetadata databaseMetadata = DatabaseMetadata.getDatabaseMetadata(jdbcTemplate.getDataSource());
+
         Dialect dialect = getDialect();
         log.info("创建表格使用[{}]方言", properties.getDatabase());
-        
+
         // 获取候选类
         Set<Class<?>> candidateClasses = getCandidateClasses();
-        
-        List<TableMetadata> tables = getTables(candidateClasses, dialect);
-        
-        for (TableMetadata table : tables) {
-            String ddlScript = SqlHelper.createTable(table);
-            jdbcTemplate.execute(ddlScript);
-            log.info("数据库表[{}]创建成功", table.tableName());
+
+        List<TableMetadata> entityConvertedTables = getTables(candidateClasses, dialect);
+
+        for (TableMetadata table : entityConvertedTables) {
+            schemaContext.handle(dialect, databaseMetadata, table);
         }
     }
-    
+
     /**
      * 获取候选class
      *
@@ -75,10 +89,10 @@ public class TableGenerate {
         Reflections reflections = new Reflections(properties.getEntityPackage());
         Set<Class<?>> tableNameAnnotationClass = reflections.getTypesAnnotatedWith(TableName.class);
         Set<Class<?>> tableAnnotationClass = reflections.getTypesAnnotatedWith(Table.class);
-        
+
         return Sets.union(tableAnnotationClass, tableNameAnnotationClass);
     }
-    
+
     /**
      * 获取方言
      *
@@ -86,57 +100,84 @@ public class TableGenerate {
      */
     private Dialect getDialect() {
         String database = properties.getDatabase();
-        
+
         if ("mysql".equalsIgnoreCase(database)) {
-            return new MySQLDialect();
+            return new MySQLDialect(properties.getCustom().getOrDefault("mysql", new HashMap<>()));
         }
-        
+
         throw new UnsupportedDatabaseException(String.format("不支持的数据库类型: %s", database));
     }
-    
+
     private List<TableMetadata> getTables(Set<Class<?>> candidateClasses, Dialect dialect) {
         List<TableMetadata> tables = new ArrayList<>();
-        
+
         TableManager tableManager = TableManager.getInstance();
-        ColumnManager columnManager = ColumnManager.getInstance(dialect);
+        ColumnManager columnManager = ColumnManager.getInstance();
         for (Class<?> tableClass : candidateClasses) {
-            
+
             TableMetadata tableMetadata = tableManager.handle(tableClass);
             Set<Field> fields = ReflectionUtils.getAllFields(tableClass, filter());
-            
+
             for (Field field : fields) {
                 ColumnMetadata columnMetadata = columnManager.handle(field);
-                
-                if (columnMetadata.primary()) {
-                    ColumnMetadata primaryColumn = tableMetadata.primaryColumn();
+
+                if (columnMetadata.getPrimary()) {
+                    ColumnMetadata primaryColumn = tableMetadata.getPrimaryColumn();
                     if (primaryColumn != null) {
-                        throw new RuntimeException(String.format("%s中有多于一个的主键,请检查实体类",
-                                                                 tableClass.getName()));
+                        throw new RuntimeException(
+                            String.format("%s中有多于一个的主键,请检查实体类", tableClass.getName()));
                     }
-                    tableMetadata.primaryColumn(columnMetadata);
+                    tableMetadata.setPrimaryColumn(columnMetadata);
                     continue;
                 }
                 tableMetadata.addColumnMetadata(columnMetadata);
+                columnMetadata.setTable(tableMetadata);
             }
             tables.add(tableMetadata);
         }
         return tables;
     }
-    
+
     private Predicate<? super Field> filter() {
         return field -> {
             boolean exists = !field.getName().equals("serialVersionUID");
-            
+
             if (field.isAnnotationPresent(TableField.class)) {
                 TableField tableField = field.getAnnotation(TableField.class);
                 exists = tableField.exist() && exists;
             }
-            
+
             if (field.isAnnotationPresent(Column.class)) {
                 Column column = field.getAnnotation(Column.class);
                 exists = !column.ignore() && exists;
             }
             return exists;
         };
+    }
+
+    private DatabaseMetadata getDatabaseMetadata(DataSource dataSource) throws SQLException {
+
+        return DatabaseMetadata.getDatabaseMetadata(dataSource);
+    }
+
+    private SchemaStrategy getSchemaStrategy(String ddlAuto) {
+        SchemaStrategy strategy = null;
+        switch (ddlAuto) {
+            case "none":
+                break;
+            case "create":
+                strategy = new SchemaCreatorStrategyImpl();
+                break;
+            case "update":
+                strategy = new SchemaUpdateStrategyImpl();
+                break;
+            case "validate":
+                strategy = new SchemaValidateStrategyImpl();
+                break;
+            default:
+                throw new IllegalArgumentException(String.format("不支持的ddl-auto方式[%s]", ddlAuto));
+        }
+
+        return strategy;
     }
 }
