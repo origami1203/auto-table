ace17e0e4d2d02a6925af1f38a87d937991d7945
origami <mcxyml123@163.com>
2023-08-28 12:33:08 +0800

    refactor: 修改DateType的length类型为int

--- src/main/java/org/origami/table/auto/dialect/DateType.java
+++ src/main/java/org/origami/table/auto/dialect/DateType.java
@@ -1,16 +1,18 @@
 package org.origami.table.auto.dialect;
 
 /**
+ * 数据库数据类型
+ *
  * @author origami
  * @date 2023/8/15 21:47
  */
 public interface DateType {
-    
+
     String getTemplate();
-    
-    Long getLength();
-    
+
+    Integer getLength();
+
     Integer getScale();
-    
+
     Integer getPrecision();
 }
