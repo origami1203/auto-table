0c54b157e7d21b609d86888a384c7b9d48530215
origami <mcxyml123@163.com>
2023-08-28 12:45:10 +0800

    refactor: 策略直接执行，不在返回sql

--- src/main/java/org/origami/table/auto/schema/SchemaContext.java
+++ src/main/java/org/origami/table/auto/schema/SchemaContext.java
@@ -3,8 +3,7 @@ package org.origami.table.auto.schema;
 import org.origami.table.auto.core.DatabaseMetadata;
 import org.origami.table.auto.core.TableMetadata;
 import org.origami.table.auto.dialect.Dialect;
-
-import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * 策略上下文
@@ -13,18 +12,20 @@ import java.util.List;
  * @date 2023/8/16 19:41
  */
 public class SchemaContext {
-    
+
     private final SchemaStrategy strategy;
-    
-    private SchemaContext(SchemaStrategy strategy) {
+    private final JdbcTemplate jdbcTemplate;
+
+    private SchemaContext(SchemaStrategy strategy, JdbcTemplate jdbcTemplate) {
         this.strategy = strategy;
+        this.jdbcTemplate = jdbcTemplate;
     }
-    
-    public List<String> getSQLString(Dialect dialect, DatabaseMetadata databaseMetadata, TableMetadata tableMetadata) {
-        return strategy.getSQLString(dialect, databaseMetadata, tableMetadata);
+
+    public void handle(Dialect dialect, DatabaseMetadata databaseMetadata, TableMetadata tableMetadata) {
+        strategy.handle(jdbcTemplate, dialect, databaseMetadata, tableMetadata);
     }
-    
-    public static SchemaContext of(SchemaStrategy strategy) {
-        return new SchemaContext(strategy);
+
+    public static SchemaContext of(SchemaStrategy strategy, JdbcTemplate jdbcTemplate) {
+        return new SchemaContext(strategy, jdbcTemplate);
     }
 }
