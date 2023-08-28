dc6f39183bf07efb017074961cc3be0978950dc7
origami <mcxyml123@163.com>
2023-08-28 12:42:44 +0800

    feat: columnResolve不在直接生成actualTypeName,而是生成jdbcTypeCode

--- src/main/java/org/origami/table/auto/resolve/DefaultColumnTypeResolver.java
+++ src/main/java/org/origami/table/auto/resolve/DefaultColumnTypeResolver.java
@@ -1,10 +1,8 @@
 package org.origami.table.auto.resolve;
 
 import com.google.common.base.CaseFormat;
-import com.google.common.base.Strings;
-import lombok.RequiredArgsConstructor;
 import org.origami.table.auto.core.ColumnMetadata;
-import org.origami.table.auto.dialect.Dialect;
+import org.origami.table.auto.utils.JDBCTypeHelper;
 
 import java.lang.reflect.Field;
 
@@ -12,19 +10,16 @@ import java.lang.reflect.Field;
  * @author origami
  * @date 2023/8/15 19:41
  */
-@RequiredArgsConstructor
 public class DefaultColumnTypeResolver implements ColumnAnnotationResolver {
-    
-    private final Dialect dialect;
-    
+
     @Override
     public ColumnMetadata resolve(Field field) {
-        String actualTypeName = dialect.getActualTypeName(field.getType(), null, null, null);
+        Integer jdbcTypeCode = JDBCTypeHelper.getJdbcTypeCodeByClassType(field.getType());
         String columnName = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, field.getName());
-        
-        return new ColumnMetadata().setColumnName(columnName).setActualTypeName(actualTypeName);
+
+        return new ColumnMetadata().setColumnName(columnName).setJdbcType(jdbcTypeCode);
     }
-    
+
     @Override
     public boolean supports(Field field) {
         return true;
